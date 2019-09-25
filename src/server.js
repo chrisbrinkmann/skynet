951f53cbb1281406e0b3e8b992c6de35fb182a3e
chrisbrinkmann <brinkmcd@gmail.com>
2019-09-25 14:09:03 -0400

    added "hello world" express server

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello world from server.')
+})
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`)
+})

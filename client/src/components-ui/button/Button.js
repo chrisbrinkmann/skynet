659be67573f02633ccefbf2c71c61dffd52c835a
Min Ko <minpko@gmail.com>
2019-10-02 14:57:18 -0400

    Add Button.js component logic to components-ui folder

---
+++ client/src/components-ui/button/Button.js
@@ -0,0 +1,25 @@
+import React, { } from 'react';
+import style from './button.module.scss';
+
+// *************************** BUTTON COMPONENT *************************** //
+const Button = ({ small, inverted, success, danger, warning, children, ...otherProps }) => {
+  // destructured props will be passed down via parent component (determines size / color / children / otherProps)
+  // size and color variables can be chained together (ie. small success)
+  return (
+    <button 
+      className={`
+        ${small ? style.small : ''}
+        ${inverted ? style.inverted : ''}
+        ${success ? style.success : ''}
+        ${danger ? style.danger : ''}
+        ${warning ? style.warning : ''}
+        ${style.button}
+      `}
+      {...otherProps}
+    >
+      {children}
+    </button>
+  )
+};
+
+export default Button;

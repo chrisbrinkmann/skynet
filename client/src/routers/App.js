00b9b85ec6c71b67d96f5fdd1bf948f088100e24
mko5118 <minpko@gmail.com>
2019-10-04 10:45:16 -0400

    Setup base routes for client side page navigation

--- client/src/routers/App.js
+++ client/src/routers/App.js
@@ -1,11 +1,22 @@
 import React, { } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import LandingPage from '../pages/LandingPage';
+import NewsfeedPage from '../pages/NewsfeedPage';
+import FriendsPage from '../pages/FriendsPage';
+import ProfilePage from '../pages/ProfilePage';
 
 // *************************** APP ROUTER *************************** //
 const App = () => {
   return (
-    <div>
-      Welcome to Skynet
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={LandingPage} />
+        <Route exact path='/newsfeed' component={NewsfeedPage} />
+        <Route exact path='/friends' component={FriendsPage} />
+        <Route exact path='/profile/:id' component={ProfilePage} />
+      </Switch>
+    </BrowserRouter>
   )
 };
 

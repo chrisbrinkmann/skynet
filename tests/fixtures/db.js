e0dd71e126cf96c01bfbf6007c5957f26dcf1409
chrisbrinkmann <brinkmcd@gmail.com>
2019-10-04 14:48:43 -0400

    Add login user api endpoint and tests

--- tests/fixtures/db.js
+++ tests/fixtures/db.js
@@ -1,12 +1,25 @@
 const db = require('../../src/database/database')
 const User = require('../../src/models/User')
+const bcrypt = require('bcryptjs')
 
 // sample user data
-const userOne = {
-  name: 'Joni Stubbs',
-  email: 'jstubbs@example.com',
-  password: '123456'
-}
+const sampleUsers = [
+  {
+    name: 'Chris',
+    email: 'chris@example.com',
+    password: '123456'
+  },
+  {
+    name: 'Min',
+    email: 'min@example.com',
+    password: '123456'
+  },
+  {
+    name: 'Peter',
+    email: 'peter@example.com',
+    password: '123456'
+  }
+]
 
 const syncDatabase = async () => {
   // for each model: drop table if exists, then create new table
@@ -17,11 +30,24 @@ const populateTables = async () => {
   // delete all rows in table
   await User.destroy({ where: {} })
 
-  // insert a sample user
-  await User.create(userOne)
+  await Promise.all(
+    // loop thru sampleUsers
+    sampleUsers.map(async user => {
+      // cache the hashed passwords
+      const hashedPassword = await bcrypt.hash(user.password, 8)
+
+      // insert sample users to DB with the hashed passwords
+      await User.create({
+        name: user.name,
+        email: user.email,
+        password: hashedPassword
+      })
+    })
+  )
 }
 
 module.exports = {
   syncDatabase,
-  populateTables
+  populateTables,
+  sampleUsers
 }

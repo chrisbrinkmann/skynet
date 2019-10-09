ffc6ec4da3a930f37ce3929054352c39a34d4670
mko5118 <minpko@gmail.com>
2019-10-09 10:28:38 -0400

    Update App.js to import NotFoundPage component into application routes

--- client/src/routers/App.js
+++ client/src/routers/App.js
@@ -5,6 +5,7 @@ import LandingPage from '../pages/LandingPage';
 import NewsfeedPage from '../pages/NewsfeedPage';
 import FriendsPage from '../pages/FriendsPage';
 import ProfilePage from '../pages/ProfilePage';
+import NotFoundPage from '../pages/404-page/NotFoundPage';
 
 // *************************** APP ROUTER *************************** //
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
         <Route exact path='/newsfeed' component={NewsfeedPage} />
         <Route exact path='/friends' component={FriendsPage} />
         <Route exact path='/profile/:id' component={ProfilePage} />
+        <Route component={NotFoundPage} />
       </Switch>
     </BrowserRouter>
   )

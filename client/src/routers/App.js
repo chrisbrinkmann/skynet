a6c76fb2304dc294720b28b44c8230441fcf5c88
mko5118 <minpko@gmail.com>
2019-10-15 19:54:36 -0400

    Update App.js in client/src/routers folder to include PrivateRoute for authenticated routes

--- client/src/routers/App.js
+++ client/src/routers/App.js
@@ -9,6 +9,7 @@ import ProfilePage from '../pages/ProfilePage';
 import NotFoundPage from '../pages/404-page/NotFoundPage';
 
 import Alert from '../components-page/alert/Alert';
+import PrivateRoute from '../components-page/private-route/PrivateRoute';
 
 // *************************** APP ROUTER *************************** //
 const App = () => {
@@ -18,9 +19,9 @@ const App = () => {
       <Alert />
       <Switch>
         <Route exact path='/' component={LandingPage} />
-        <Route exact path='/newsfeed' component={NewsfeedPage} />
-        <Route exact path='/friends' component={FriendsPage} />
-        <Route exact path='/profile/:id' component={ProfilePage} />
+        <PrivateRoute exact path='/newsfeed' component={NewsfeedPage} />
+        <PrivateRoute exact path='/friends' component={FriendsPage} />
+        <PrivateRoute exact path='/profile/:id' component={ProfilePage} />
         <Route component={NotFoundPage} />
       </Switch>
     </BrowserRouter>

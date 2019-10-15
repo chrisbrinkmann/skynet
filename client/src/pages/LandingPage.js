da52b714d574e29d8afbeef6e77b72c4fc3a6476
Min Ko <minpko@gmail.com>
2019-10-15 13:29:18 -0400

    Update LandingPage.js to connect with redux store to redirect to NewsfeedPage if authenticated

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,11 +1,18 @@
 import React, { } from 'react';
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import PageContainer from '../components-page/page-container/PageContainer';
 import LandingText from '../components-page/landing-text/LandingText';
 import SignUp from '../components-page/sign-up/SignUp';
 
 // *************************** LANDING PAGE *************************** //
-const LandingPage = () => {
+const LandingPage = ({ isAuthenticated }) => {
+  if (isAuthenticated) {
+    // If user is authenticated, Redirect to NewsfeedPage.js
+    return <Redirect to='/newsfeed' />
+  }
+
   return (
     <PageContainer landing>
       
@@ -16,4 +23,9 @@ const LandingPage = () => {
   )
 };
 
-export default LandingPage;
+// REDUX
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps)(LandingPage);

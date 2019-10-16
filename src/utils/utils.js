b79877a8ae3e958f2ff61cc4f4a43ea8c32159f5
chrisbrinkmann <brinkmcd@gmail.com>
2019-10-16 15:29:20 -0400

    Implement and refactor helper functions for use in api routes

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,6 +2,8 @@ const jwt = require('jsonwebtoken')
 const gravatar = require('gravatar')
 const User = require('../models/User')
 const Relation = require('../models/Relation')
+const Post = require('../models/Post')
+const Comment = require('../models/Comment')
 const Sequelize = require('sequelize')
 const Op = Sequelize.Op
 const { check } = require('express-validator')
@@ -68,17 +70,17 @@ const setAvatar = email => {
 }
 
 // returns a properly formatted object for insertion to relations table
-const formatPendingRelation = (requester, requestee, relationType) => {
-  // cache variables; assume requester has the lower id
+const formatPendingRelation = (first_user_id, second_user_id, relationType) => {
+  // cache variables; assume first_user_id has the lower id
   let relationData = {
-    first_user_id: requester,
-    second_user_id: requestee,
+    first_user_id,
+    second_user_id,
     relationType
   }
-  // if requester has the higher id; swap user positions and relation type
-  if (requester > requestee) {
-    relationData.first_user_id = requestee
-    relationData.second_user_id = requester
+  // if first_user_id has the higher id; swap user positions and relation type
+  if (first_user_id > second_user_id) {
+    relationData.first_user_id = second_user_id
+    relationData.second_user_id = first_user_id
     relationData.relationType = 'pending_second_first'
   }
 
@@ -139,19 +141,35 @@ const getFriendRelations = async user_id => {
   return friends
 }
 
+// returns an array integer friend ids of user
+const getFriendIds = async user_id => {
+  const friendRelations = await getFriendRelations(user_id)
+  let friend_ids = []
+
+  // add friend ids to array cache
+  friendRelations.map(relation => {
+    if (relation.first_user_id !== user_id) {
+      return friend_ids.push(relation.first_user_id)
+    }
+    if (relation.second_user_id !== user_id) {
+      return friend_ids.push(relation.second_user_id)
+    }
+  })
+
+  return friend_ids
+}
+
 // returns a formatted friends list array with friend id, name, avatar
 const populateFriendsList = async (friendRelations, user_id) => {
+  // cache friend ids
+  let friend_ids = await getFriendIds(user_id)
+
   // create cache of friend objects
   let friendsList = []
 
   // add object with friend id to friendsList for each friend
-  friendRelations.map(friend => {
-    if (friend.first_user_id !== user_id) {
-      return friendsList.push({ id: friend.first_user_id })
-    }
-    if (friend.second_user_id !== user_id) {
-      return friendsList.push({ id: friend.second_user_id })
-    }
+  friend_ids.map(id => {
+    return friendsList.push({ id })
   })
 
   // add friend user.name and user.avatar to each friendsList object
@@ -169,6 +187,89 @@ const populateFriendsList = async (friendRelations, user_id) => {
   return friendsList
 }
 
+// returns newsfeed array of formatted post objects
+const populateNewsFeed = async user_id => {
+  // cache friend ids, include self
+  let friend_ids = await getFriendIds(user_id)
+  friend_ids.push(user_id)
+
+  // query db for all ids of posts by friends and self
+  const friendPosts = await Post.findAll({
+    where: {
+      user_id: {
+        [Op.in]: friend_ids
+      }
+    },
+    order: [['id', 'DESC']],
+    attributes: ['id'],
+    limit: 100
+  })
+
+  // for each post get add the formatted post to the array
+  const newsFeed = Promise.all(
+    friendPosts.map(async post => {
+      return await getFormattedPost(post.dataValues.id)
+    })
+  )
+
+  return newsFeed
+}
+
+// returns a formatted post object with comments array
+const getFormattedPost = async post_id => {
+  const post = await Post.findOne({ where: { id: post_id } })
+  const user = await User.findOne({ where: { id: post.dataValues.user_id } })
+
+  return {
+    id: post.dataValues.id,
+    user_id: user.dataValues.id,
+    user_name: user.dataValues.name,
+    user_avatar: user.dataValues.avatar,
+    content: post.dataValues.content,
+    comments: await getFormattedPostComments(post_id)
+  }
+}
+
+// returns array of formatted comments for a post
+const getFormattedPostComments = async postId => {
+  // query db for all comments with post_id matching the param
+  const comments = await Comment.findAll({
+    where: { post_id: postId },
+    order: [['id', 'ASC']],
+    attributes: ['id'],
+    limit: 100
+  })
+
+  // for each comment add the formatted comment to the array
+  const postComments = Promise.all(
+    comments.map(async comment => {
+      return await getFormattedComment(comment.dataValues.id)
+    })
+  )
+
+  return postComments
+}
+
+// returns a formatted comment object
+const getFormattedComment = async comment_id => {
+  const comment = await Comment.findOne({
+    where: { id: comment_id },
+    attributes: ['id', 'user_id', 'content']
+  })
+  const user = await User.findOne({
+    where: { id: comment.dataValues.user_id },
+    attributes: ['id', 'name', 'avatar']
+  })
+
+  return {
+    id: comment.dataValues.id,
+    user_id: user.dataValues.id,
+    user_name: user.dataValues.name,
+    user_avatar: user.dataValues.avatar,
+    content: comment.dataValues.content
+  }
+}
+
 module.exports = {
   createAuthToken,
   createUser,
@@ -180,5 +281,7 @@ module.exports = {
   areFriends,
   getRelation,
   getFriendRelations,
-  populateFriendsList
+  getFriendIds,
+  populateFriendsList,
+  populateNewsFeed
 }

5d3bfb86cfb37c1b3b2fcc1598feafa943e3856c
mko5118 <minpko@gmail.com>
2019-10-18 13:46:43 -0400

    Update NewsfeedPage.js to render Posts component

--- client/src/pages/NewsfeedPage.js
+++ client/src/pages/NewsfeedPage.js
@@ -2,6 +2,7 @@ import React, { } from 'react';
 
 import PageContainer from '../components-page/page-container/PageContainer';
 import PostCreate from '../components-page/post-create/PostCreate';
+import Posts from '../components-page/posts/Posts';
 
 // *************************** NEWSFEED PAGE *************************** //
 const NewsfeedPage = () => {
@@ -9,6 +10,7 @@ const NewsfeedPage = () => {
     <PageContainer>
       
       <PostCreate />
+      <Posts />
 
     </PageContainer>
   )

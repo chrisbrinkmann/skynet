95bec2e29da643a6e3901ab11426ac72d9673ddf
Min Ko <minpko@gmail.com>
2019-10-17 21:05:58 -0400

    Update NewsfeedPage.js to import PostCreate component

--- client/src/pages/NewsfeedPage.js
+++ client/src/pages/NewsfeedPage.js
@@ -1,11 +1,15 @@
 import React, { } from 'react';
+
 import PageContainer from '../components-page/page-container/PageContainer';
+import PostCreate from '../components-page/post-create/PostCreate';
 
 // *************************** NEWSFEED PAGE *************************** //
 const NewsfeedPage = () => {
   return (
     <PageContainer>
-      Newsfeed Page
+      
+      <PostCreate />
+
     </PageContainer>
   )
 };

c2b83345623abc324ad2270a2df61e58f730ec02
chrisbrinkmann <brinkmcd@gmail.com>
2019-10-18 22:24:43 -0400

    Test deploy setup

--- src/database/database.js
+++ src/database/database.js
@@ -1,16 +1,21 @@
 const Sequelize = require('sequelize')
 
+let useSSL = true
+
+// set use SSL to false if NODE_ENV is development or test
+if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
+  useSSL = false
+}
+
 // connect to db
-const db = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    logging: false
+const db = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: useSSL
   }
-)
+})
 
 // test db connection
 db.authenticate()

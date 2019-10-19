e06b5878cc8f6e8f619244263023883d1c382c5c
chrisbrinkmann <brinkmcd@gmail.com>
2019-10-18 23:41:22 -0400

    Deploy build script and serve static assets

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
+const path = require('path')
 
 // midleware
 app.use(express.json())
@@ -11,5 +12,16 @@ app.use('/users', require('./routes/users'))
 app.use('/relations', require('./routes/relations'))
 app.use('/posts', require('./routes/posts'))
 app.use('/comments', require('./routes/comments'))
+
+// serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'))
+
+  // serve the built index.js
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 module.exports = app

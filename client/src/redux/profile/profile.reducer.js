156636940d31675d68f9cb1ae39e3842995136f4
mko5118 <minpko@gmail.com>
2019-10-20 17:44:51 -0400

    Add profile setup folder and files to redux

---
+++ client/src/redux/profile/profile.reducer.js
@@ -0,0 +1,13 @@
+
+const INITIAL_STATE = {
+  currentProfile: null,
+  loading: true,
+  error: {},
+};
+
+export const profileReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default :
+      return state;
+  };
+};

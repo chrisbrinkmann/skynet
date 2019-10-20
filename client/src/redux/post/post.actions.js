87c55576f23b13d69d47d23666bb72f8e78775bc
mko5118 <minpko@gmail.com>
2019-10-20 15:14:48 -0400

    Update deletePost -- post.actions.js -- was causing re-render problems upon deletion of post

--- client/src/redux/post/post.actions.js
+++ client/src/redux/post/post.actions.js
@@ -35,24 +35,26 @@ export const addPost = ({ content }) => async (dispatch) => {
 
 // *************************** DELETE POST *************************** //
 export const deletePost = (postId) => async (dispatch) => {
-  try {
-    if (window.confirm('Please confirm you want to delete this post. This action cannot be undone.')) {
-      const res = await axios.delete(`${route}/posts/${postId}`);
+  if (window.confirm('Please confirm you want to delete this post. This action cannot be undone.')) {
+    try {
+      await axios.delete(`${route}/posts/${postId}`);
       dispatch({
         type: DELETE_POST,
-        payload: res.data,
+        payload: postId,
       });
-      dispatch(setAlert('Post Successfully Deleted', 'success', 2000));
-    }
-  } catch (err) {
-    dispatch({
-      type: POST_ERROR,
-      payload: {
-        msg: err.response.statusText,
-        status: err.response.status,
-      }
-    });
-  }
+      dispatch(setAlert('Post deleted successfully', 'success', 2000));
+    } catch (err) {
+      console.error(err);
+      dispatch({
+        type: POST_ERROR,
+        payload: {
+          msg: err.response.statusText,
+          status: err.response.status, 
+        }
+      });
+      dispatch(setAlert('Error deleting post', 'danger', 2000));
+    };
+  };
 };
 
 // *************************** GET ALL POSTS *************************** //

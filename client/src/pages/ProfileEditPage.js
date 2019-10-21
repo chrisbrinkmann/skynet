c1b8849351b60feeda860a8365ac35ddd9adb085
mko5118 <minpko@gmail.com>
2019-10-21 11:13:59 -0400

    Add ProfileEditPage component to pages folder

---
+++ client/src/pages/ProfileEditPage.js
@@ -0,0 +1,17 @@
+import React, { } from 'react';
+
+import PageContainer from '../components-page/page-container/PageContainer';
+import ProfileEdit from '../components-page/profile-edit/ProfileEdit';
+
+// *************************** PROFILE EDIT PAGE *************************** //
+const ProfileEditPage = () => {
+  return (
+    <PageContainer>
+      
+      <ProfileEdit />
+
+    </PageContainer>
+  )
+};
+
+export default ProfileEditPage;

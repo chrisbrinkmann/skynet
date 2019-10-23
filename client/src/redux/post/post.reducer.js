b251409f233459ba9ca8a5bda2b3416d096638c1
mko5118 <minpko@gmail.com>
2019-10-23 14:44:44 -0400

    Remove components and redux logic related to getAllPosts as it is not a core component -- was initially used for testing post functionality

--- client/src/redux/post/post.reducer.js
+++ client/src/redux/post/post.reducer.js
@@ -1,4 +1,4 @@
-import { ADD_POST, DELETE_POST, GET_NEWSFEED, GET_POSTS, ADD_COMMENT, DELETE_COMMENT, POST_ERROR } from './post.types';
+import { ADD_POST, DELETE_POST, GET_NEWSFEED, ADD_COMMENT, DELETE_COMMENT, POST_ERROR } from './post.types';
 
 const INITIAL_STATE = {
   posts: [],
@@ -29,12 +29,6 @@ export const postReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         newsfeed: action.payload,
-        loading: false,
-      };
-    case GET_POSTS :
-      return {
-        ...state,
-        posts: action.payload,
         loading: false,
       };
     case ADD_COMMENT :

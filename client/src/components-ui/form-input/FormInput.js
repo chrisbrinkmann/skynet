db92a4ffad049d9ce6d5df4f5b57031c1a79a186
mko5118 <minpko@gmail.com>
2019-10-26 14:54:00 -0400

    Update FormInput component and styling to include 'search' prop

--- client/src/components-ui/form-input/FormInput.js
+++ client/src/components-ui/form-input/FormInput.js
@@ -2,12 +2,13 @@ import React, { } from 'react';
 import style from './form-input.module.scss';
 
 // *************************** FORM INPUT COMPONENT *************************** //
-const FormInput = ({ small, onChange, ...otherProps }) => {
+const FormInput = ({ small, search, onChange, ...otherProps }) => {
   // destructured props passed down via parent component (size / onChange handler / otherProps)
   return (
     <input 
       className={`
         ${small ? style.small : ''}
+        ${search ? style.search : ''}
         ${style.input}
       `}
       onChange={onChange}

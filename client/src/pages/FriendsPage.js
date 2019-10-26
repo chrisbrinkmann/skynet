5b6c1b23a9616ac8f702dacdb6259ea66421b42c
mko5118 <minpko@gmail.com>
2019-10-25 22:39:33 -0400

    Update FriendsPage.js with Friends component import

--- client/src/pages/FriendsPage.js
+++ client/src/pages/FriendsPage.js
@@ -1,11 +1,15 @@
 import React, { } from 'react';
+
 import PageContainer from '../components-page/page-container/PageContainer';
+import Friends from '../components-page/friends/Friends';
 
 // *************************** FRIENDS PAGE *************************** //
 const FriendsPage = () => {
   return (
     <PageContainer>
-      Friends Page
+
+      <Friends />
+
     </PageContainer>
   )
 };
